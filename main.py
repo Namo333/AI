9c3aaac25ac06ded2d43a246bf391f4c9ce2234e
Namo333 <dad688634@gmail.com>
2024-04-19 12:08:32 +0300

    stryc

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+from components.voice_processing import *
+
+# load_dotenv()
+# TOKEN = os.getenv("TOKEN")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
